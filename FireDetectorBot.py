901ea26536415998c27111790dc5cd851c0a2149
nestoor22 <45801713+nestoor22@users.noreply.github.com>
2019-05-29 13:27:02 +0300

    Add files via upload

--- FireDetectorBot.py
+++ FireDetectorBot.py
@@ -1,10 +1,11 @@
 from telegram.ext import Updater, CommandHandler
 import psycopg2 as sq
 from threading import Thread
+from PIL import Image
+import io
 
 
 BOT_TOKEN = "800792656:AAF3UcFpElvjeG3q3b-Q9JjRVSEn_c_Y6JE"
-PATH_TO_PHOTO = 'check_photo/check.png'
 database = sq.connect('postgres://lzfllcfvvyspsq:ce5e2cb0b0e63af2a6afef0a4077018567ce8638750d9f7'
                       '56c76b86e78255a9c@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432'
                       '/ddmeddrk3fhuln', sslmode='require')
@@ -33,27 +34,35 @@ class TelegramUsersDataBase:
             self.telegram_database.commit()
         except:
             self.telegram_database.rollback()
-            print("SORRY")
 
     def add_referral(self, referrer_id, referral_id):
         try:
             self.cursor.execute("""UPDATE telegram_user SET user_referrer = %s WHERE user_referrer is NULL AND
-                                chat_id = %s""", (referrer_id, referral_id,))
+                                    chat_id = %s""", (referrer_id, referral_id,))
             self.telegram_database.commit()
         except:
             self.telegram_database.rollback()
 
     def select_referrals(self, referral_id):
-        self.cursor.execute("""SELECT chat_id FROM telegram_user WHERE user_referrer = %s""",
-                                       (referral_id,))
+        self.cursor.execute("""SELECT chat_id FROM telegram_user WHERE user_referrer = %s"""
+                            , (referral_id,))
         return self.cursor.fetchall()
+
+    def get_img(self, chat_id):
+        try:
+            self.cursor.execute("""SELECT img FROM telegram_user WHERE chat_id = %s""",
+                                (chat_id,))
+            return self.cursor.fetchone()[0]
+        except:
+            self.telegram_database.rollback()
+            return 0
 
     @staticmethod
     def check_user_exist(chat_id):
         try:
             cursor = database.cursor()
             cursor.execute("""SELECT EXISTS(SELECT 1 FROM telegram_user
-                                           WHERE chat_id = %s)""",(chat_id,))
+                                            WHERE chat_id = %s)""", (chat_id,))
             return cursor.fetchone()[0]
         except:
             database.rollback()
@@ -97,8 +106,8 @@ class TelegramBot:
         else:
             try:
                 self.bot_users_database().add_new_user(update.message.from_user.first_name,
-                                                           update.message.from_user.last_name,
-                                                           update.message.from_user.username,chat_id)
+                                                       update.message.from_user.last_name,
+                                                       update.message.from_user.username,chat_id)
                 bot.sendMessage(chat_id=chat_id, text="Hello, {0}".format(update.message.from_user.first_name))
             except:
                 bot.sendMessage(chat_id=chat_id, text="Please, try later")
@@ -107,19 +116,28 @@ class TelegramBot:
 
     def send_check_photo(self, bot, update):
         chat_id = update.message.chat_id
-        photo = open(PATH_TO_PHOTO, 'rb')
-        bot.sendPhoto(chat_id=chat_id, photo=photo)
+        photo = self.bot_users_database().get_img(chat_id)
+        if photo == 0:
+            bot.sendMessage(chat_id=chat_id, text='Please, try later')
+        photo_file = io.BytesIO(photo)
+        image = Image.open(photo_file)
+        photo_file.name = 'image.jpg'
+        image.save(photo_file, 'JPEG')
+        photo_file.seek(0)
+        bot.sendPhoto(chat_id=chat_id, photo=photo_file)
         referrals_id = self.bot_users_database().select_referrals(referral_id=chat_id)
         if len(referrals_id) > 0:
-            for chat_id in referrals_id:
-                Thread(target=self.send_to_referrals, args=(bot, chat_id[0])).start()
+            for ref_id in referrals_id:
+                Thread(target=self.send_to_referrals, args=(bot, ref_id[0], chat_id,)).start()
 
-    @staticmethod
-    def send_to_referrals(bot, chat_id):
-        pass
-        # photo = open(PATH_TO_PHOTO, 'rb')
-        # bot.sendPhoto(chat_id=chat_id, photo=photo)
-        # photo.close()
+    def send_to_referrals(self, bot, ref_id, chat_id):
+        photo = self.bot_users_database().get_img(chat_id)
+        photo_file = io.BytesIO(photo)
+        image = Image.open(photo_file)
+        photo_file.name = 'image.jpg'
+        image.save(photo_file, 'JPEG')
+        photo_file.seek(0)
+        bot.sendPhoto(chat_id=ref_id, photo=photo_file)
 
     def user_settings(self, bot, update):
         pass
@@ -131,6 +149,4 @@ class TelegramBot:
 
 if __name__ == '__main__':
     x = TelegramBot()
-    z = (x.bot_users_database().select_referrals(434234))
-    for i in z:
-        print(i[0])
+    x.start_bot()
